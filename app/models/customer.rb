52dd76931354c1c6c179d5b7f4f568b59e6e1fe3
Yuutamu <iwamoto.yuta1925@gmail.com>
2023-12-25 20:13:18 +0900

    Customer,Admin Models 実装

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,17 @@
 class Customer < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # customize validation of Devise.gem
+  with_options presence: true do
+    validates :name
+    validates :status
+  end
+
+  # Customer Status
+  enum status: {
+    normal: 0,    # 通常
+    withdrawn: 1, # 退会済み
+    banned: 2     # アカウント停止（運営判断による）
+  }
 end
--- db/migrate/20231225105032_devise_create_customers.rb
+++ db/migrate/20231225105032_devise_create_customers.rb
@@ -32,6 +32,8 @@ class DeviseCreateCustomers < ActiveRecord::Migration[7.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :name, null: false
+      t.integer :status, null: false, default: 0
 
       t.timestamps null: false
     end
