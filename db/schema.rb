e3efdffba1fae751c1c9d71de2c5be3324677c3e
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-01-08 13:06:49 +0900

    Productテーブル migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,8 +1,19 @@
-ActiveRecord::Schema[7.1].define(version: 2023_12_25_105032) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_01_08_040302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  # devise.gem を利用
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -15,7 +26,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_25_105032) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  # devise.gem を利用
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -28,6 +38,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_25_105032) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.integer "stock", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
