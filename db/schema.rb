92e5b0015e2a54e600b48d68c0f25eb3f82de8f9
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-01-11 01:01:10 +0900

    CartItem models  generate

--- db/schema.rb
+++ db/schema.rb
@@ -1,7 +1,19 @@
-ActiveRecord::Schema[7.1].define(version: 2024_01_10_085131) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_01_10_155605) do
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  # アップロードしたファイルデータと Active Record を紐付ける（中間テーブル）, 目的：画像投稿機能
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -12,7 +24,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_10_085131) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  # アップロードしたファイルを保存する（BLOB型）, 目的：画像投稿機能
   create_table "active_storage_blobs", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
@@ -25,7 +36,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_10_085131) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  # アップロードしたファイルのデータの variant に関する情報を保存する, 目的：画像投稿機能
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
@@ -42,6 +52,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_10_085131) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "quantity", default: 1, null: false
+    t.bigint "customer_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_cart_items_on_customer_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
   end
 
   create_table "customers", force: :cascade do |t|
@@ -69,4 +89,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_10_085131) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cart_items", "customers"
+  add_foreign_key "cart_items", "products"
 end
