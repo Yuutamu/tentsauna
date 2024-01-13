ab783e2057ad0084233ea046ab94a6924bfd2adb
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-01-13 14:50:00 +0900

    order details generate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_13_044950) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_13_054727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,12 +78,23 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_13_044950) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  create_table "order_details", force: :cascade do |t|
+    t.integer "price", null: false
+    t.integer "quantity", null: false
+    t.bigint "order_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_details_on_order_id"
+    t.index ["product_id"], name: "index_order_details_on_product_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.string "name", null: false
     t.string "postal_code", null: false
     t.string "prefecture", null: false
     t.string "address1", null: false
-    t.string "address2" # null でもOK
+    t.string "address2"
     t.integer "postage", null: false
     t.integer "billing_amount", null: false
     t.integer "status", default: 0, null: false
@@ -106,5 +117,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_13_044950) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cart_items", "customers"
   add_foreign_key "cart_items", "products"
+  add_foreign_key "order_details", "orders"
+  add_foreign_key "order_details", "products"
   add_foreign_key "orders", "customers"
 end
