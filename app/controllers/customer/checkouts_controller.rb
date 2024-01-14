32f40927a2269e3c4f7fcb8b916d69a5bd2c0e01
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-01-14 18:01:45 +0900

    Stripe Checkout の実装

--- app/controllers/customer/checkouts_controller.rb
+++ app/controllers/customer/checkouts_controller.rb
@@ -1,2 +1,42 @@
+# Stripe 特有の書き方が多い（公式Doc:https://stripe.com/docs/api/checkout/sessions/object）
 class Customer::CheckoutsController < ApplicationController
+  before_action :authenticate_customer!
+
+  def create
+    # line_items == "顧客が購入した品目"
+    line_items = current_customer.line_items_checkout
+    session = create_session(line_items)
+
+    # 現在のホストとは異なるホストへのリダイレクトを許可する StripeCheckoutページ
+    redirect_to session.url, allow_other_host: true
+  end
+
+  private
+
+  def create_session(line_items)
+    Stripe::Checkout::Session.create(
+      client_reference_id: current_customer.id, # チェックアウトセッションを参照するためのユニークな文字列。 顧客IDとして利用
+      customer_email: current_customer.email,
+      mode: 'payment', # CheckOut Session Mode (payment, setup, subscription がある)
+      payment_method_types: ['card'], # クレカ支払いのみOK
+      line_items:,
+      shipping_address_collection: {
+        allowed_countries: ['JP']
+      },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: 'fixed_amount', # 全商品の送料固定
+            fixed_amount: {
+              amount: 3000, # 送料
+              currency: 'jpy'
+            },
+            display_name: 'Single rate'
+          }
+        }
+      ],
+      success_url: root_url, # サンクスページ
+      cancel_url: "#{root_url}cart_items" # 決済キャンセル時
+    )
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # customize validation of Devise.gem
+  # customize validations of Devise.gem
   with_options presence: true do
     validates :name
     validates :status
@@ -17,4 +17,23 @@ class Customer < ApplicationRecord
 
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
+
+  # メモ：オブジェクトのプロパティは、Stripe 側で指定されている
+  def line_items_checkouts
+    cart_items.map do |cart_item|
+      {
+        quantity: cart_item.quantity,
+        price_data: {
+          currency: 'jpy',
+          unit_amount: cart_item.product.price,
+          product_data: {
+            name: cart_item.product.name,
+            metadata: {
+              product_id: cart_item.product_id
+            }
+          }
+        }
+      }
+    end
+  end
 end
