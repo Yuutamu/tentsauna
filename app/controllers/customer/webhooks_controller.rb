689f6b19890463a5ced492ba7cfe938e48018415
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-01-16 01:54:38 +0900

    webhooks_controller 実装

--- app/controllers/customer/webhooks_controller.rb
+++ app/controllers/customer/webhooks_controller.rb
@@ -39,11 +39,16 @@ class Customer::WebhooksController < ApplicationController
 
       # トランザクション処理の開始(用いる理由：処理の１つで例外が発生した場合に、その処理を含んだ全ての処理を巻き戻すことができるのでrollback)
       ApplicationRecord.transaction do
-        order = create_order(session) # session を元にOrderテーブルにデータ代入
-        # order_details テーブルにデータを挿入する処理 を書く
+        order = create_order(session) # session を元にOrder テーブルにデータ代入
+        # メモ：expand に関して（Stripe公式：https://stripe.com/docs/expand）
+        session_with_expand = Stripe::Checkout::Session.retrive({ id: session.id, expand: ['line_items'] })
+        session_with_expand.line_items.data.each do |line_item|
+          create_order_details(order, line_item) # retrive で取り出したline_item をOrder_details テーブルに代入
+        end
       end
       # トランザクション処理の終了
 
+      customer.cart_items.destroy_all # customerのカートない商品を全て削除
       redirect_to session.success_url
     end
   end
@@ -53,16 +58,29 @@ class Customer::WebhooksController < ApplicationController
   # 参考（Stripe Session Object 一覧：https://stripe.com/docs/api/checkout/sessions/object）
   def create_order(session)
     Order.create!({
-      customer_id: session.client_reference_id,
-      name: session.shipping_details.name,
-      postal_code: session.shipping_details.address.postal_code,
-      prefecture: session.shipping_details.address.state,
-      address1: session.shipping_details.address.line1,
-      address2: session.shipping_details.address.line2,
-      postage: session.shipping_options[0].shipping_amount,
-      billing_amount: session.amount_total,
-      status: 'confirm_payment' # メモ：'checkout.session.completed' イベントを受信した時点で、決済は完了しているのでステータスを「入金済み」へ
-    })
+                    customer_id: session.client_reference_id,
+                    name: session.shipping_details.name,
+                    postal_code: session.shipping_details.address.postal_code,
+                    prefecture: session.shipping_details.address.state,
+                    address1: session.shipping_details.address.line1,
+                    address2: session.shipping_details.address.line2,
+                    postage: session.shipping_options[0].shipping_amount,
+                    billing_amount: session.amount_total,
+                    status: 'confirm_payment' # メモ：'checkout.session.completed' イベントを受信した時点で、決済は完了しているのでステータスを「入金済み」へ
+                  })
   end
 
+  def create_order_details(order, line_item)
+    product = Stripe::Product.retrive(line_item.price.product)
+    purchased_product = Product.find(product.metadata.product_id) # メモ：Stripe の metadata はダッシュボードから確認可能
+
+    raise ActiveRecord::RecordNotFound if purchased_product.nil? # メモ：意図的に例外エラーを発生させる
+
+    order_detail = order.order_details.create!({
+                                                 product_id: purchased_product.id,
+                                                 price: line_item.price.unit_amount,
+                                                 quantity: line_item.quantity
+                                               })
+    purchased_product.update!(stock: (purchased_product.stock - order_detail.quantity)) # 購入商品の在庫数更新
+  end
 end
