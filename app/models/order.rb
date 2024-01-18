ebbcd69f5a22c099bf5df900870016e154b25cbe
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-01-19 00:08:04 +0900

    models/order 本日注文分のscopeを作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,9 +9,11 @@ class Order < ApplicationRecord
   }
   has_many :order_details, dependent: :destroy
 
-  scope :waiting_payment, -> {where(status: 'waiting_payment') }
-  scope :confirm_payment, -> {where(status: 'confirm_payment') }
-  scope :shipped, -> {where(status: 'shipped') }
-  scope :out_of_deliverly, -> {where(status: 'out_of_deliverly') }
-  scope :delivered, -> {where(status: 'delivered') }
+  scope :waiting_payment, -> { where(status: 'waiting_payment') }
+  scope :confirm_payment, -> { where(status: 'confirm_payment') }
+  scope :shipped, -> { where(status: 'shipped') }
+  scope :out_of_deliverly, -> { where(status: 'out_of_deliverly') }
+  scope :delivered, -> { where(status: 'delivered') }
+
+  scope :created_today, -> { where('orders.created_at >= ?', Time.zone.now.biginning_of_day) } # Time.zone.now.beginning_of_day 一日の始まりの00:00を返す
 end
