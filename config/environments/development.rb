25becc59c7cf4db082109a5e620727d942a98156
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-01-30 01:44:55 +0900

    メールサーバ設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,22 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # アプリケーションのホスト情報を設定
+  config.action_mailer.default_url_options = { host: 'localhost', port: 8000 }
+
+  # メールの送信方式を SMTP（Simple Mail Transfer Protocol）に設定
+  config.action_mailer.delivery_method = :smtp
+
+  # SMTP設定
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: Rails.application.credentials.dig(:gmail, :email),
+    password: Rails.application.credentials.dig(:gmail, :app_password),
+    authentication: :login
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
