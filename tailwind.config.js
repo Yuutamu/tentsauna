d5afc7355cada9364c239cb781172a0792dff070
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-02-15 14:00:51 +0900

    tailwind 利用カラーの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,13 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark-green': '#4B5049',
+        'light-green': '#669988'
+      }
+    }
+  }
 }
