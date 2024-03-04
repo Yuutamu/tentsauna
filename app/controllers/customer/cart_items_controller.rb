e6d9d31d7f9e3d4c9c7241b70c091bba2b2339d6
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-03-04 18:20:06 +0900

    メソッド名

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -12,13 +12,13 @@ class Customer::CartItemsController < ApplicationController
     redirect_to cart_items_path, notice: 'カートに商品を追加しました。'
   end
 
-  # メモ：通常CRUD処理以外のメソッド名。一般的な動詞にした
+  # MEMO：通常CRUD処理以外のメソッド名。一般的な動詞にした
   def increase
     @cart_item.increment!(:quantity, 1)
     redirect_to request.referer, notice: 'カート商品を更新しました。'
   end
 
-  # メモ：通常CRUD処理以外のメソッド名。一般的な動詞にした
+  # MEMO：通常CRUD処理以外のメソッド名。一般的な動詞にした
   def decrease
     decrease_or_destroy(@cart_item)
     redirect_to request.referer, notice: 'カート商品を更新しました。'
@@ -35,17 +35,17 @@ class Customer::CartItemsController < ApplicationController
     @cart_item = current_customer.cart_items.find(params[:id])
   end
 
-  # メモ：増加の際はincrement | 新規作成時はcreate
+  # MEMO：増加の際はincrement | 新規作成時はcreate
   def increase_or_create(product_id)
     cart_item = current_customer.cart_items.find_by(product_id:)
     if cart_item
       cart_item.increment!(:quantity, 1)
     else
-      current_customer.cart_items.build(product_id:).save # メモ：createにしないことでdebugしやすくさせる
+      current_customer.cart_items.build(product_id:).save # MEMO：createにしないことでdebugしやすくさせる
     end
   end
 
-  # メモ：減少の際はdecrement | カート削除時はdestroy
+  # MEMO：減少の際はdecrement | カート削除時はdestroy
   def decrease_or_destroy(cart_item)
     if cart_item.quantity > 1
       cart_item.decrement!(:quantity, 1)
