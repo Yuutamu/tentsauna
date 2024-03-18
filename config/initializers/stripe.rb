faff123369c2f4efb91f21faeb37a4df7caaf49a
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-03-19 04:45:03 +0900

    stripe credentials に関して補足

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,4 @@
+# MEMO: VIエディタよりcredentialsに登録した内容をdigメソッドで参照する（参照：https://spirits.appirits.com/doruby/9473/）
 Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
-# api_vesion 最新確認（公式：https://stripe.com/docs/api/versioning?lang=ruby）
+# MEMO: api_vesion 最新確認（公式：https://stripe.com/docs/api/versioning?lang=ruby）
 Stripe.api_version = '2023-10-16'
