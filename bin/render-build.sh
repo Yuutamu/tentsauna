754ffe5b58e3d7b9bf1fa819dd882d592d3f8dd1
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-03-19 20:27:57 +0900

    yarn install 記述

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,8 @@
 set -o errexit
 
 bundle install
-# yarn install
-# yarn build # jsファイルをesbuildでバンドルしているため
+yarn install
+yarn build # jsファイルをesbuildでバンドルしているため
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
